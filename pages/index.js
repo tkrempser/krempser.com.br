3926d360afd513e61e2506916acedf5b493bf199
Thiago Krempser <tkrempser@gmail.com>
2021-08-30 18:03:58 -0300

    Update pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+import { Fragment } from "react";
+
+import Typography from "@material-ui/core/Typography";
+
+import Emoji from "../components/Emoji";
+
 const Home = () => {
-  return <h1>Welcome!</h1>;
+  return (
+    <Fragment>
+      <Typography variant="h2" component="h1" gutterBottom>
+        Hello! <Emoji symbol="👋" label="waving-hand" />
+      </Typography>
+
+      <Typography variant="h5" component="h2" gutterBottom>
+        My name is Thiago Krempser. I am a Full Stack Developer from Brazil.
+      </Typography>
+    </Fragment>
+  );
 };
 
 export default Home;
---
+++ pages/projects/index.js
@@ -0,0 +1,5 @@
+const About = () => {
+  return <h1>Projects</h1>;
+};
+
+export default About;
