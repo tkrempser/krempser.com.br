acddfe09b80fc371065239fc32957c8992ba2928
Thiago Krempser <tkrempser@gmail.com>
2021-08-30 18:03:34 -0300

    Update Next app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import "../styles/globals.css";
+import classes from "../styles/app.module.css";
+import Copyright from "../components/Copyright";
 
-export default MyApp
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+
+      <Container maxWidth="md" component="main" className={classes.main}>
+        <Component {...pageProps} />
+      </Container>
+
+      <Container maxWidth="md" component="footer" className={classes.footer}>
+        <Copyright
+          siteTitle="Thiago Krempser"
+          siteUrl="https://krempser.com.br"
+        />
+      </Container>
+    </div>
+  );
+};
+
+export default MyApp;
