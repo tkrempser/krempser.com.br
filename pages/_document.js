07536b938e9ad147771653be06cd0777ffb7c012
Thiago Krempser <tkrempser@gmail.com>
2021-08-31 01:06:36 -0300

    Update document file

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width"
-          />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
