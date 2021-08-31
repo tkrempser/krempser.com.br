2b6d49b2579d14eaa23966a15b0a1481746fd4ea
Thiago Krempser <tkrempser@gmail.com>
2021-08-31 16:39:16 -0300

    Add Layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,11 @@
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Container from "@material-ui/core/Container";
-import Head from "next/head";
-
 import "../styles/globals.css";
-import classes from "../styles/app.module.css";
-import Copyright from "../components/Copyright";
+import Layout from "../components/Layout";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-
-      <Head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <title>Thiago Krempser</title>
-      </Head>
-
-      <Container maxWidth="md" component="main" className={classes.main}>
-        <Component {...pageProps} />
-      </Container>
-
-      <Container maxWidth="md" component="footer" className={classes.footer}>
-        <Copyright
-          siteTitle="Thiago Krempser"
-          siteUrl="https://krempser.com.br"
-        />
-      </Container>
-    </div>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 };
 
