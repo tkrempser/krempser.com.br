0a47f7a9235674c0fe7dc93b0142bfe8ce29b013
Thiago Krempser <tkrempser@gmail.com>
2022-08-23 14:21:57 -0300

    Update novak project description

--- components/ProjectList.js
+++ components/ProjectList.js
@@ -21,7 +21,7 @@ const projects = [
   {
     id: "2",
     title: "novak",
-    description: "Automated betting based on mathematical analysis models using Python and Django.",
+    description: "Automated betting based on mathematical analysis models using Python, Django and Celery.",
     image: "https://krempser.com.br/images/novak.jpg",
     codeUrl: "",
     liveDemoUrl: "https://novakbot.com/",
